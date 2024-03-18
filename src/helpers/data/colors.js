e2c00303715546773d27ee83495f68ebc8e41079
kiritoEM <johankirito64@gmail.com>
2024-03-18 17:12:31 +0100

    add lottie and text

---
+++ src/helpers/data/colors.js
@@ -0,0 +1,7 @@
+export default function colors() {
+    const blue = "#4385E7";
+    const beige = "#F2DEBF";
+    const green = "#14EB76";
+
+    return { blue, beige, green }
+}
--- src/screens/welcoming/Welcoming.jsx
+++ src/screens/welcoming/Welcoming.jsx
@@ -2,27 +2,30 @@ import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
 import Shapes from "../../components/auth-components/Shapes";
 import LottieView from 'lottie-react-native';
 import { useFonts, Montserrat_500Medium, Montserrat_700Bold, Montserrat_600SemiBold } from "@expo-google-fonts/montserrat";
+import colors from "../../helpers/data/colors";
 
 const Welcoming = () => {
     const [fontsLoaded] = useFonts({
         Montserrat_500Medium, Montserrat_600SemiBold, Montserrat_700Bold
     });
 
+    const { blue } = colors();
+
     if (!fontsLoaded) {
         return <Text>Loading...</Text>;
     }
 
     return (
-        <View style={welcomingStyles.container}>
-            <Shapes>
+        <Shapes>
+            <View style={welcomingStyles.container}>
                 <View>
                     <LottieView source={require('./../../assets/Animation - 1710688901899.json')} style={welcomingStyles.lottie} autoPlay />
                 </View>
-                <View>
-                    <Text style={{ fontSize: 26, fontFamily: "Montserrat_700Bold" }}>Apprendre en ligne</Text>
+                <View style={welcomingStyles.header}>
+                    <Text style={{ fontSize: 26.5, fontFamily: "Montserrat_600SemiBold" }}>Apprendre en ligne avec <Text style={{ color: `${blue}`, fontFamily: "Montserrat_700Bold" }}>LearnShare!</Text></Text>
                 </View>
-            </Shapes>
-        </View>
+            </View>
+        </Shapes>
     );
 };
 
@@ -33,11 +36,19 @@ const welcomingStyles = StyleSheet.create({
         height: height,
         backgroundColor: "#fff",
         display: "flex",
-        justifyContent: "center"
+        justifyContent: "center",
+        gap: 20
     },
     lottie: {
         height: 310,
         objectFit: "contain"
+    },
+    header: {
+        width: width,
+        display: "flex",
+        gap: 20,
+        paddingLeft: 27,
+        paddingRight: 27
     }
 })
 
