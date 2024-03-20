c0cc7d0a8bdab3f5d23292b380102899b53e9d12
kiritoEM <johankirito64@gmail.com>
2024-03-20 20:38:00 +0100

    add buttons in google-auth

--- src/components/auth-components/GoogleAuth.jsx
+++ src/components/auth-components/GoogleAuth.jsx
@@ -1,25 +1,42 @@
-import { Montserrat_500Medium, useFonts } from "@expo-google-fonts/montserrat";
+import { Montserrat_400Regular, Montserrat_500Medium, useFonts } from "@expo-google-fonts/montserrat";
 import { Fragment } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import constants from "../../helpers/data/constants";
 
 const GoogleAuth = () => {
     const [fontsLoaded] = useFonts({
+        Montserrat_500Medium,
         Montserrat_500Medium
     });
+
+
+    if (!fontsLoaded) {
+        return <Text>Loading...</Text>
+    }
+
     return (
         <Fragment>
-            <View style={styles.container}>
+            <View style={styles.or_styles}>
                 <View style={styles.line} />
                 <Text style={styles.text}>Ou se connecter avec</Text>
                 <View style={styles.line} />
+            </View>
+            <View style={styles.buttons}>
+                <View style={styles.button}>
+                    <Image source={require('./../../assets/google-icon.png')} style={{ width: 20, height: 20, objectFit: "contain" }} />
+                    <Text style={{ fontFamily: "Montserrat_500Medium", fontSize: 12.5 }}>Google</Text>
+                </View>
+                <View style={styles.button}>
+                    <Image source={require('./../../assets/facebook-icon.png')} style={{ width: 20, height: 20, objectFit: "contain" }} />
+                    <Text style={{ fontFamily: "Montserrat_500Medium", fontSize: 12.5 }}>Google</Text>
+                </View>
             </View>
         </Fragment>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
+    or_styles: {
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
@@ -33,7 +50,27 @@ const styles = StyleSheet.create({
     line: {
         flex: 1,
         height: 1,
-        backgroundColor: constants.colors.gray_01,
+        backgroundColor: "rgba(139, 136, 136, 0.4)",
+    },
+    buttons: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 17
+    },
+    button: {
+        flex: 1,
+        borderColor: "rgba(139, 136, 136, 0.4)",
+        borderWidth: 1,
+        backgroundColor: "#FAFAFA",
+        paddingHorizontal: 11,
+        height: 48,
+        borderRadius: constants.radius,
+        display: "flex",
+        flexDirection: "row",
+        display: "flex",
+        alignItems: "center",
+        gap: 10
     }
 });
 
